f2f8ea3c7fe53d9c3de2b63f092737573ab2247b
Joseph D. Ewbank <jewbank@conroeisd.net>
2019-12-03 13:49:58 -0600

    Refactored to have Brains pick move rather than position

--- JBrainTetris.java
+++ JBrainTetris.java
@@ -1,27 +1,58 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class JBrainTetris extends JTetris {
 	
+	private static final int movesPerTick = 1;
 	private Brain brain;
+	protected javax.swing.Timer brainTimer;
 	
 	JBrainTetris(int width, int height) {
 		super(width, height);
 		brain = new LameBrain();
+		
+		brainTimer = new javax.swing.Timer(DELAY/movesPerTick, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				executeBrainMove();
+			}
+		});
 	}
+	
+	@Override
+	public void startGame()
+	{
+		super.startGame();
+		brainTimer.start();
+	}
+	
+	@Override
+	public void stopGame()
+	{
+		brainTimer.stop();
+		super.stopGame();
+	}
+	
+	@Override
+	public void updateTimer()
+	{
+		super.updateTimer();
+		double value = ((double)speed.getValue())/speed.getMaximum();
+		brainTimer.setDelay((int)(DELAY - value*DELAY)/movesPerTick);
+	}
+	
+	@Override
 	public void tick(int verb)
+	{
+		moved = false;
+		super.tick(verb);
+	}
+	
+	public void executeBrainMove()
 	{
 		if (currentPiece != null) {
 			board.undo();	// remove the piece from its old position
 		}
-
-		Brain.Move move = brain.bestMove(board, currentPiece, HEIGHT + TOP_SPACE, null);
-
-		if(!currentPiece.equals(move.piece))
-			super.tick(ROTATE);
-		if(move.x < currentX)
-			super.tick(LEFT);
-		else if(move.x > currentX)
-			super.tick(RIGHT);
-		super.tick(DOWN);
+		int verb = brain.bestMove(board,currentPiece,currentX,currentY,HEIGHT);
+		tick(verb);
 	}
-	
 }
